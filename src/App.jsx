83bbe5e6421192799ce634a4eab498eca2c767ea
Aarik Maharjan <mhrznaarik870@gmail.com>
2025-03-17 16:01:31 +0545

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import NetflixSeries, { Footer, Header } from "./components/NetflixSeries";
 import '@mantine/core/styles.css';
 
 import { MantineProvider } from '@mantine/core';
+import { EventHandling } from "./components/EventHandling";
 
 export default function App() {
   return <MantineProvider>{
     <section className="container">
       <h1 className="card-heading">List of Top Netflix Series</h1>
-      <NetflixSeries />
+      {/* <NetflixSeries /> */}
+      <EventHandling />
     </section>
   }
   </MantineProvider>;
