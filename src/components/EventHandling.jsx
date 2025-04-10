ba51bac0bfbab1a34c825fe5ccb9a366b6ecdd47
Aarik Maharjan <mhrznaarik870@gmail.com>
2025-04-10 12:46:12 +0545

    eventHandling updated

--- src/components/EventHandling.jsx
+++ src/components/EventHandling.jsx
@@ -1,42 +1,28 @@
 export const EventHandling = () => {
 
+    // function handleButtonClick() {   //normal function
+    //     alert(`Hey! You clicked Click Me!`)
+    // }
+
     const handleButtonClick = (event) => {
         console.log(event);
         console.log(event.target);
         console.log(event.type);
-        alert("Hey! I am onClick Event");
+        alert(`Hey, You clicked Click Me!`);
     }
 
-    const handleWelcomeUser = (users) => {
-        console.log(`Hey ${users}, Welcome`);
+    const handleWelcomeUser = (user) => {
+        console.log(`Hey ${user}, You clicked welcome`)
     }
     return (
         <>
             <button onClick={handleButtonClick}>Click Me!</button>
-            <br />
-            <br />
+            <button onClick={(event) => handleButtonClick(event)}>Click Me-2!</button>
+            <button onClick={(event) => console.log(event)}>Inline function</button>
+            <button onClick={() => alert(`You clicked Inline arrow function`)}> Inline event</button>
 
-            <button onClick={(event) => handleButtonClick(event)}>2-Click Me!</button>
-            <br />
-            <br />
-
-            <button onClick={(event) => console.log(event)}>Inline Function</button>
-            <br />
-            <br />
-
-            <button onClick={() => alert("Hey I am inline event function")}>Inline Arr function</button>
-            <br />
-            <br />
-
-            <button onClick={handleWelcomeUser}>Click Me!!</button>
-            <br />
-            <br />
-
-            <button onClick={() => handleWelcomeUser("Venom")}>Click Me!!</button>
-            <br />
-            <br />
-
-            <button onClick={() => handleWelcomeUser("Ram")}>Click Me!!</button>
+            <button onClick={() => handleWelcomeUser("Venom")}>Welcome</button>
+            <button onClick={() => handleWelcomeUser("Ram")}>Welcome</button>
         </>
     )
-}
+}   
