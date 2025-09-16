8d767ea078967b2d60f9e808a33e6aec0d578b73
Aarik Maharjan <mhrznaarik870@gmail.com>
2025-09-16 17:01:35 +0545

    feat: add DarkLight component and ThemeProvider for theme toggling

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,12 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  theme: {
+    extend: {
+      fontFamily: {
+        display: ["Poppins"],
+      }
+    }
+  }
 })
